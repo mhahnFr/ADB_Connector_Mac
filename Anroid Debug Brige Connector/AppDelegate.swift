64caad4b5fe663b53c5517e02bda07f34c915da8
Manuel Hahn <manuel.hahn@orange.fr>
2019-06-24 09:59:01 +0200

    Initial Commit

---
+++ Anroid Debug Brige Connector/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Anroid Debug Brige Connector
+//
+//  Created by Manuel Hahn on 24.06.19.
+//  Copyright © 2019 Manuel Hahn. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Anroid Debug Brige ConnectorTests/Anroid_Debug_Brige_ConnectorTests.swift
@@ -0,0 +1,34 @@
+//
+//  Anroid_Debug_Brige_ConnectorTests.swift
+//  Anroid Debug Brige ConnectorTests
+//
+//  Created by Manuel Hahn on 24.06.19.
+//  Copyright © 2019 Manuel Hahn. All rights reserved.
+//
+
+import XCTest
+@testable import Anroid_Debug_Brige_Connector
+
+class Anroid_Debug_Brige_ConnectorTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
